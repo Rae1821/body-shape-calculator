6d2e5b488c844a627340845ad24d66d079ad5561
rachel_dooley <rae1821@gmail.com>
2022-12-07 13:12:39 -0500

    Additional updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-const form = document.querySelector("form");
+const form = document.querySelector(".form");
 const shoulders = document.getElementById("shoulders");
-const bust = document.getElementById("bust");
 const waist = document.getElementById("waist");
 const hips = document.getElementById("hips");
 
@@ -8,6 +7,13 @@ const result = document.querySelector(".result");
 const tips = document.querySelector(".tips");
 const button = document.querySelector(".calculate");
 const error = document.querySelector(".error");
+const buttonStartOver = document.querySelector(".start-over");
+
+const pear = document.querySelector(".pear");
+const apple = document.querySelector(".apple");
+const hourglass = document.querySelector(".hourglass");
+const inverted = document.querySelector(".inverted");
+const rectangle = document.querySelector(".rectangle");
 
 
 
@@ -25,26 +31,58 @@ button.addEventListener("click", function(e) {
        error.innerText = "Please fill in all the fields"
        return;
    } else if (hipValue / shoulderValue >= 1.05) {
-        result.innerText = "You have a Pear Body Shape.";
+        result.innerText = "You have a Pear Body Shape!";
+        pear.classList.remove("hide");
+        pear.classList.add("show");
     } else if (shoulderValue / hipValue >= 1.05 && waistValue === shoulderValue) {
-        result.innerText = "You have an Apple Body Shape";
+        result.innerText = "You have an Apple Body Shape!";
+        apple.classList.remove("hide");
+        apple.classList.add("show");
     } else if (waistValue / shoulderValue <= 0.75 && waistValue / hipValue <= 0.75 && (hipValue * 0.95) < shoulderValue) {
-        result.innerText = "You have an Hourglass Body Shape";
+        result.innerText = "You have an Hourglass Body Shape!";
+        hourglass.classList.remove("hide");
+        hourglass.classList.add("show");
     } else if (shoulderValue / hipValue >= 1.05 && waistValue < shoulderValue) {
-        result.innerText = "You have an Inverted Triangle Body Shape";
+        result.innerText = "You have an Inverted Triangle Body Shape!";
+        inverted.classList.remove("hide");
+        inverted.classList.add("show");
     } else if (waistValue / shoulderValue >= 0.75 && (shoulderValue * 0.95) < hipValue) {
-        result.innerText = "You have a Rectangle Body Shape";
+        result.innerText = "You have a Rectangle Body Shape!";
+        rectangle.classList.remove("hide");
+        rectangle.classList.add("show");
     } else {
         error.innerText = "Please fill in all fields";
     }
     
     //Clear the form
     clearForm(); 
+
+    //Show the start over button
+    startOver();
    
 });
 
-
+//Clear the form function
 const clearForm = function() {
     form.reset();
     error.innerText = "";
+   
 };
+
+//Show the start over button function
+function startOver() {
+    button.classList.add("hide");
+    buttonStartOver.classList.remove("hide");
+    buttonStartOver.classList.add("show");
+}
+
+//When click the start over button the start over button disappears and the calculate button reappears
+buttonStartOver.addEventListener("click", function(){
+    button.classList.remove("hide");
+    buttonStartOver.classList.remove("show");
+    buttonStartOver.classList.add("hide");
+    tips.classList.remove("show");
+    tips.classList.add("hide");
+    
+})
+
